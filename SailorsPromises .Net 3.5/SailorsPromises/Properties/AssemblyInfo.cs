7a3e4d75664a65c81a3cbc4f551b329031243d3a
Matteo Canessa <matcanessa@gmail.com>
2014-07-09 00:51:36 +0200

    .Net 3.5 version added

---
+++ SailorsPromises .Net 3.5/SailorsPromises/Properties/AssemblyInfo.cs
@@ -0,0 +1,65 @@
+﻿// <copyright file="AssemblyInfo.cs" company="https://github.com/matteocanessa/SailorsPromises">
+//     Copyright (c) 2014 Matteo Canessa (sailorspromises@gmail.com)
+// </copyright>
+// <summary>Promise interface</summary>
+//
+// The MIT License (MIT)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#region Using directives
+
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+#endregion
+
+[assembly: AssemblyTitle("SailorsPromises")]
+[assembly: AssemblyDescription("A free and open-source library for the .NET Framework to make asynchronous calls more friendly")]
+
+#if DEBUG
+[assembly: AssemblyProduct("SailorsPromises - Debug version")]
+#else
+[assembly: AssemblyProduct("SailorsPromises")]
+#endif
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("Copyright 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// This sets the default COM visibility of types in the assembly to invisible.
+// If you need to expose a type to COM, use [ComVisible(true)] on that type.
+[assembly: ComVisible(false)]
+
+// The assembly version has following format :
+//
+// Major.Minor.Build.Revision
+//
+// You can specify all the values or you can use the default the Revision and 
+// Build Numbers by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.*")]
+
+[assembly: CLSCompliant(true)]
+
+[assembly: InternalsVisibleTo("SailorsPromisesTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100EFBA8D97E4BCBE57853A5712D5EEA2253FEFC0061535FAD5A3F778CFA402350C95BC589491DC20BF98BC2202A0E003A8CFEE37BCA27399568062E533193AE2A7DE6E950AE1FE4806275D2B676BF783B6C01FA080D37899B5B110F598FC44F30615E901FD326D3376440161B2C6CE63A522814D352D4DC1C63A6F09A822C64BD2")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2, PublicKey=0024000004800000940000000602000000240000525341310004000001000100c547cac37abd99c8db225ef2f6c8a3602f3b3606cc9891605d02baa56104f4cfc0734aa39b93bf7852f7d9266654753cc297e7d2edfe0bac1cdcf9f717241550e0a7b191195b7667bb4f64bcb8e2121380fd1d9d46ad2d92d2d15605093924cceaf74c4861eff62abf69b9291ed0a340e113be11e6a7d3113e92484cf7045cc7")] 
---
+++ SailorsPromises .Net 3.5/SailorsPromisesTests/SailorTestsSuite.cs
@@ -0,0 +1,182 @@
+﻿//The MIT License (MIT)
+//
+//Copyright (c) 2014 Matteo Canessa (sailorspromises@gmail.com)
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included in
+//all copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//THE SOFTWARE.
+
+using System;
+using System.Threading;
+using SailorsPromises;
+using Xunit;
+
+namespace SailorsPromisesTests
+{
+    /// <summary>
+    /// Description of SailorTestsSuite.
+    /// </summary>
+    public class SailorTestsSuite
+    {
+        [Fact]
+        public void Resolve_method_should_call_promise_fulfill_exactly_once()
+        {
+            string val = "iyhbiyhb";
+           
+            PromiseMock promiseMock = new PromiseMock();
+
+            Sailor d = new Sailor(promiseMock);
+            d.Resolve(val);
+
+            Assert.Equal(1, promiseMock.FulfillCalls);
+        }
+
+        [Fact]
+        public void Reject_method_should_call_promise_reject_exactly_once()
+        {
+            Exception exc = new Exception();
+
+            PromiseMock promiseMock = new PromiseMock();
+            
+            Sailor d = new Sailor(promiseMock);
+            d.Reject(exc);
+
+            Assert.Equal(1, promiseMock.RejectCalls);
+        }
+
+        [Fact]
+        public void Finally_method_should_call_promise_finally_exactly_once()
+        {
+            int call = 0;
+
+            Sailor d = new Sailor();
+            d.Promise.Finally(delegate() { call++; });
+            d.Finally();
+
+            //Finally method should call promise finally exactly once
+            Assert.Equal(1, call);
+        }
+
+        [Fact]
+        public void Notify_method_should_call_promise_notify_exactly_once()
+        {
+            string val = "iyhbiyhb";
+            int call = 0;
+
+            Sailor d = new Sailor();
+            d.Promise.Notify(delegate(object obj) { call++; });
+            d.Notify(val);
+
+            //Notify method should call promise notify exactly once
+            Assert.Equal(1, call);
+        }
+        
+        [Fact]
+        public void When_method_should_run_the_Action_and_all_others_methods_async_on_another_thread()
+        {
+            string currentThreadName = Thread.CurrentThread.Name;
+            
+            Sailor d = new Sailor();
+            
+            //When method should be executed on a thread other than the calling one
+            d.When(delegate() { Assert.NotEqual(Thread.CurrentThread.Name, currentThreadName); })
+                //Then method should be executed on a thread other than the calling one
+                .Then(delegate(object obj) { Assert.NotEqual(Thread.CurrentThread.Name, currentThreadName); })
+                //Finally method should be executed on a thread other than the calling one
+                .Finally(delegate() { Assert.NotEqual(Thread.CurrentThread.Name, currentThreadName); })
+                //Let the test fail if there is an exception
+                .OnError(delegate(Exception exc) { Assert.False(true); });
+        }
+        
+        [Fact]
+        public void When_method_should_call_Then_Action_when_the_Action_passed_to_it_comlpetes_without_errors()
+        {
+            int callNr = 0;
+            int callNr1 = 0;
+
+            Sailor d = new Sailor();
+            d.When(delegate() { Thread.Sleep(500); })
+                .Then(delegate(object obj) { callNr++; })
+                .OnError(delegate(Exception exc) { callNr1++; });
+            
+            Thread.Sleep(1000);
+            
+            //When method should call Then Action when the Action passed to it comlpetes without errors
+            Assert.Equal(1, callNr);
+            Assert.Equal(0, callNr1);
+        }
+        [Fact]
+        public void When_method_should_call_Finally_action_when_the_Action_passed_to_it_comlpetes_without_errors()
+        {
+            int callNr = 0;
+            int callNr1 = 0;
+
+            Sailor d = new Sailor();
+            d.When(delegate() { Thread.Sleep(500); })
+                .Finally(delegate() { callNr++; })
+                .OnError(delegate(Exception exc) { callNr1++; });
+            
+            Thread.Sleep(1000);
+
+            //When method should call Finally action when the Action passed to it comlpetes without errors
+            Assert.Equal(1, callNr);
+            Assert.Equal(0, callNr1);
+        }
+        
+        [Fact]
+        public void When_method_should_call_Finally_action_when_the_Action_passed_to_it_comlpetes_with_errors()
+        {
+            int callNr = 0;
+            int callNr1 = 0;
+            int callNr2 = 0;
+
+            Sailor d = new Sailor();
+            d.When(delegate() {Thread.Sleep(500); throw new Exception();})
+                .Finally(delegate() { callNr++; })
+                .OnError(delegate(Exception exc) { callNr1++; })
+                .Then(delegate(object obj) { callNr2++; });
+            
+            Thread.Sleep(1000);
+            
+            //When method should call Finally action when the Action passed to it comlpetes with errors
+            Assert.Equal(1, callNr);
+            Assert.Equal(1, callNr1);
+            Assert.Equal(0, callNr2);
+        }
+        
+        [Fact]
+        public void Then_action_should_be_called_on_the_same_current_thread_if_when_method_is_used_with_a_synch_context()
+        {
+            SynchronizationContext sc = new SynchronizationContext();
+            SynchronizationContext.SetSynchronizationContext(sc);
+            
+            string currentThreadName = Thread.CurrentThread.Name;
+
+            Sailor d = new Sailor();
+            //When method should be executed on the same calling thread if a synchronization context is set
+            d.When(delegate() { Assert.Equal(Thread.CurrentThread.Name, currentThreadName); })
+                //Then method should be executed on the same calling thread if a synchronization context is set
+                .Then(delegate(object obj) { Assert.Equal(Thread.CurrentThread.Name, currentThreadName); })
+                //Finally method should be executed on the same calling thread if a synchronization context is set
+                .Finally(delegate() { Assert.Equal(Thread.CurrentThread.Name, currentThreadName); })
+                //Let the test fail if there is an exception
+                .OnError(delegate(Exception exc) { Assert.False(true); });
+            
+            SynchronizationContext.SetSynchronizationContext(null);
+        }
+    }
+}
